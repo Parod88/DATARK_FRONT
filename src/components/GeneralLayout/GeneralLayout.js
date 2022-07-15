449b7a2d0e927eb37d4e15b2c54a02e4d9430e72
Parod88 <pabrodgom@gmail.com>
2022-07-15 12:06:14 +0200

    home page styles

---
+++ src/components/GeneralLayout/GeneralLayout.js
@@ -0,0 +1,9 @@
+function GeneralLayout({ children }) {
+  return (
+    <div id="general-layout">
+      <main>{children}</main>
+    </div>
+  );
+}
+
+export default GeneralLayout;
--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -1,16 +1,22 @@
 import React, { useEffect } from 'react';
 import './HomePage.scss';
 import HomeLogIn from '../Auth/LogIn/HomeLogIn';
+import GeneralLayout from '../GeneralLayout/GeneralLayout';
 
 function HomePage() {
   return (
-    <div id="homepage">
-      <h1 id="home-title">Welcome to creity</h1>
-      <h3 id="home-subtitle">
-        El diseño del proyecto arquitectónico como una conversación
-      </h3>
-      <HomeLogIn></HomeLogIn>
-    </div>
+    <GeneralLayout>
+      <>
+        <div class="homepage">
+          <h1 id="home-title">Welcome to</h1>
+          <h1 id="home-title2">creity</h1>
+          <h3 id="home-subtitle">
+            El diseño del proyecto arquitectónico como una conversación
+          </h3>
+          <HomeLogIn></HomeLogIn>
+        </div>
+      </>
+    </GeneralLayout>
   );
 }
 
